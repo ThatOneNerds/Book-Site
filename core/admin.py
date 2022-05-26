6273c41d9fd0e8f12a5f1dc89ad154dc35973999
Troy Merritt <troy@Troys-MacBook-Air.local>
2022-05-26 09:00:18 -0400

    made a bunch of changes, was to lazy to actually write a good message

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import book, review
 
 # Register your models here.
+
+admin.site.register(book)
+admin.site.register(review)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+#I should edit the limits for the charfield, not going to do that right now
+
+class book(models.Model):
+
+    book_name = models.CharField(max_length=50)
+    book_author = models.CharField(max_length=50)
+    book_description = models.TextField()
+    book_price = MoneyField(max_digits=14, decimal_places=2, default_currency='USD')
+
+    def __str__(self):
+        return self.book_name
+
+class review(models.Model):
+    reviewer_name = models.CharField(max_length=50)
+    pub_date = models.DateField(null=True)
+    reviewer_response = models.TextField()
+
+    def __str__(self):
+        return self.reviewer_name
