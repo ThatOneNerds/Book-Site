25b97b461f3863fb16d3a8d990a9e9b6b0023575
Guess <56052381+ThatOneNerds@users.noreply.github.com>
2022-09-03 14:48:52 -0400

    Update settings.py

--- book_review/settings.py
+++ book_review/settings.py
@@ -21,9 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w5l4w5c6d6=a6_040yvk1f_6b)c$6k0bws7ne!_=s+zm$kwu^8'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
